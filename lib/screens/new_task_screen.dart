e71c87fdf01cb5ef49defebdbd8cdf0197377f3b
gyuh312 <guanyonguh513@gmail.com>
2023-06-19 04:50:32 +0800

    add task screen done

--- lib/screens/new_task_screen.dart
+++ lib/screens/new_task_screen.dart
@@ -15,6 +15,7 @@ class _NewTaskPageState extends State<NewTaskPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _venueController = TextEditingController();
+  final TextEditingController _categoryController = TextEditingController();
   DateTime? _dueDateTime;
   late Future<String> matricNo;
 
@@ -25,6 +26,14 @@ class _NewTaskPageState extends State<NewTaskPage> {
   }
 
   @override
+  void dispose() {
+    _titleController.dispose();
+    _venueController.dispose();
+    _categoryController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final Color primaryColor = Theme.of(context).primaryColor;
     return Scaffold(
@@ -32,106 +41,225 @@ class _NewTaskPageState extends State<NewTaskPage> {
         title: Text('New Task'),
         backgroundColor: primaryColor,
       ),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _titleController,
-                decoration: InputDecoration(
-                  labelText: 'Task Title',
-                  border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _titleController,
+                  decoration: InputDecoration(
+                    labelText: 'Task Title',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a task title';
+                    }
+                    return null;
+                  },
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a task title';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                controller: _venueController,
-                decoration: InputDecoration(
-                  labelText: 'Venue',
-                  border: OutlineInputBorder(),
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: _venueController,
+                  decoration: InputDecoration(
+                    labelText: 'Venue',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a venue';
+                    }
+                    return null;
+                  },
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a venue';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(primaryColor),
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: _categoryController,
+                  decoration: InputDecoration(
+                    labelText: 'Category',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a category';
+                    }
+                    return null;
+                  },
                 ),
-                onPressed: () async {
-                  final DateTime? picked = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2099),
-                  );
-                  if (picked != null && picked != _dueDateTime) {
-                    setState(() {
-                      _dueDateTime = picked;
-                    });
-                  }
-                },
-                child: Text(
-                  _dueDateTime == null
-                      ? 'Select due date'
-                      : 'Due date: ${_dueDateTime!.toIso8601String()}',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              SizedBox(height: 10),
-              FutureBuilder<String>(
-                future: matricNo,
-                builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return CircularProgressIndicator();
-                  } else if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  } else {
-                    return ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(primaryColor),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 60,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(primaryColor),
+                          ),
+                          onPressed: () async {
+                            final DateTime? pickedDate = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(2000),
+                              lastDate: DateTime(2099),
+                              builder: (BuildContext context, Widget? child) {
+                                return Theme(
+                                  data: ThemeData.light().copyWith(
+                                    colorScheme: ColorScheme.light(
+                                      primary: Color(
+                                          0xff81163f), // Set the calendar color
+                                    ),
+                                  ),
+                                  child: child!,
+                                );
+                              },
+                            );
+
+                            if (pickedDate != null) {
+                              final TimeOfDay? pickedTime =
+                                  await showTimePicker(
+                                context: context,
+                                initialTime: TimeOfDay.now(),
+                                builder: (BuildContext context, Widget? child) {
+                                  return Theme(
+                                    data: ThemeData.light().copyWith(
+                                      colorScheme: ColorScheme.light(
+                                        primary: Color(
+                                            0xff81163f), // Set the clock color
+                                      ),
+                                    ),
+                                    child: child!,
+                                  );
+                                },
+                              );
+                              if (pickedTime != null) {
+                                setState(() {
+                                  _dueDateTime = DateTime(
+                                    pickedDate.year,
+                                    pickedDate.month,
+                                    pickedDate.day,
+                                    pickedTime.hour,
+                                    pickedTime.minute,
+                                  );
+                                });
+                              }
+                            }
+                          },
+                          child: Column(
+                            children: [
+                              Icon(Icons.calendar_today),
+                              Text(
+                                _dueDateTime == null
+                                    ? 'Select due date'
+                                    : 'Due date: ${_dueDateTime!.toIso8601String().split('T')[0]}',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                      onPressed: () async {
-                        if (_formKey.currentState!.validate()) {
-                          Task newTask = Task(
-                            title: _titleController.text,
-                            venue: _venueController.text,
-                            dueDateTime: _dueDateTime!,
-                            category: 'Category',
-                            userId: snapshot.data!,
-                            isRepeated: false,
-                            id:
-                                                        '', 
-                          );
-                          await widget.taskService.createTask(newTask);
-                          Navigator.of(context).pop();
-                        }
-                      },
-                      child: Text(
-                        'Submit',
-                        style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Container(
+                        height: 60,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(primaryColor),
+                          ),
+                          onPressed: () async {
+                            final TimeOfDay? pickedTime = await showTimePicker(
+                              context: context,
+                              initialTime: TimeOfDay.now(),
+                              builder: (BuildContext context, Widget? child) {
+                                return Theme(
+                                  data: ThemeData.light().copyWith(
+                                    colorScheme: ColorScheme.light(
+                                      primary: Color(
+                                          0xff81163f), // Set the clock color
+                                    ),
+                                  ),
+                                  child: child!,
+                                );
+                              },
+                            );
+                            if (pickedTime != null) {
+                              setState(() {
+                                _dueDateTime = DateTime(
+                                  _dueDateTime?.year ?? DateTime.now().year,
+                                  _dueDateTime?.month ?? DateTime.now().month,
+                                  _dueDateTime?.day ?? DateTime.now().day,
+                                  pickedTime.hour,
+                                  pickedTime.minute,
+                                );
+                              });
+                            }
+                          },
+                          child: Column(
+                            children: [
+                              Icon(Icons.access_time),
+                              Text(
+                                _dueDateTime == null
+                                    ? 'Select due time'
+                                    : 'Due time: ${_dueDateTime!.toIso8601String().split('T')[1].substring(0, 5)}',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                    );
-                  }
-                },
-              ),
-            ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                FutureBuilder<String>(
+                  future: matricNo,
+                  builder:
+                      (BuildContext context, AsyncSnapshot<String> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else {
+                      return ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(primaryColor),
+                        ),
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            Task newTask = Task(
+                              title: _titleController.text,
+                              venue: _venueController.text,
+                              dueDateTime: _dueDateTime!,
+                              category:
+                                  _categoryController.text, // Save the category
+                              userId: snapshot.data!,
+                              isRepeated: false,
+                              id: '',
+                            );
+                            await widget.taskService.createTask(newTask);
+                            Navigator.of(context).pop();
+                          }
+                        },
+                        child: Text(
+                          'Submit',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
